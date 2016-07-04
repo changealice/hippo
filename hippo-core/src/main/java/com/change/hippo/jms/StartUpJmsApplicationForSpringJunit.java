ca2731802512ee6f6db9d97a4436151e2c43b147
龙飞 <change.long@99bill.com>
2016-07-04 16:43:10 +0800

    nexus 3 maven configuration and spring junit support

--- hippo-core/src/main/java/com/change/hippo/jms/StartUpJmsApplicationForSpringJunit.java
+++ hippo-core/src/main/java/com/change/hippo/jms/StartUpJmsApplicationForSpringJunit.java
@@ -4,14 +4,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 
 /**
  * User: change.long
  * Date: 16/6/30
  * Time: 上午11:39
  */
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
 @ContextConfiguration(classes = JmsApplication.class)
 public class StartUpJmsApplicationForSpringJunit {
 
