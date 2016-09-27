d617dcaa0f2efac132425ef592a5b476fab21512
龙飞 <change.long@99bill.com>
2016-09-27 10:00:16 +0800

    modify

--- hippo-spring-boot/src/main/java/com/change/SampleSimpleApplication.java
+++ hippo-spring-boot/src/main/java/com/change/SampleSimpleApplication.java
@@ -2,6 +2,9 @@ package com.change;
 
 import com.change.config.ImportConfig;
 
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -12,16 +15,16 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import java.util.Arrays;
 
 /**
- * User: change.long
- * Date: 16/5/9
- * Time: 下午4:42
+ * User: change.long Date:
+ * 16/5/9 Time: 下午4:42
  */
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.change")
 @EnableScheduling
 @Import(value = ImportConfig.class)
-public class SampleSimpleApplication {
+//@MapperScan("com.change.repository.mybatis")
+public class SampleSimpleApplication implements CommandLineRunner {
     public static void main(String[] args) {
         ApplicationContext appCtx = SpringApplication.run(SampleSimpleApplication.class, args);
         String[] beanDefinitionNames = appCtx.getBeanDefinitionNames();
@@ -29,5 +32,10 @@ public class SampleSimpleApplication {
 //        for (String beanName : beanDefinitionNames) {
 //            System.out.println(beanName);
 //        }
+    }
+
+    @Override
+    public void run(String... strings) throws Exception {
+
     }
 }
--- hippo-spring-boot/src/main/java/com/change/config/ApplicationConfig.java
+++ hippo-spring-boot/src/main/java/com/change/config/ApplicationConfig.java
@@ -1,14 +1,13 @@
 package com.change.config;
 
+import com.change.runnner.MyBatisCommandRunner;
 import com.change.runnner.StartupCommandLineRunner;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * User: change.long
- * Date: 16/5/14
- * Time: 上午12:16
+ * User: change.long Date: 16/5/14 Time: 上午12:16
  */
 @Configuration
 public class ApplicationConfig {
@@ -17,6 +16,12 @@ public class ApplicationConfig {
     @Bean
     public StartupCommandLineRunner startupRunner() {
         return new StartupCommandLineRunner();
+    }
+
+
+    @Bean
+    public MyBatisCommandRunner myBatisCommandRunner() {
+        return new MyBatisCommandRunner();
     }
 
 }
