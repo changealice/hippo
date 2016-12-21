09cae33fcd40fbe8d8f04f5fd45f693faf8fc1be
龙飞 <change.long@99bill.com>
2016-12-21 17:05:34 +0800

    feign demo

---
+++ hippo-spring-boot-consumer-feign/src/main/java/com/change/spring/cloud/service/UserFeignClient.java
@@ -0,0 +1,24 @@
+package com.change.spring.cloud.service;
+
+import com.change.spring.cloud.entity.User;
+import com.netflix.appinfo.InstanceInfo;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * User: change.long@99bill.com
+ * Date: 2016/12/21
+ * Time: 下午2:57
+ */
+@FeignClient(name = "hippo-spring-boot")
+public interface UserFeignClient {
+
+    @RequestMapping("/users/{id}")
+    User findById(@PathVariable("id") Long id);
+
+
+    @RequestMapping("/eureka-instance")
+    InstanceInfo instanceInfo();
+}
