16872188919711cc5fdf4d7c23f5a1699ff9ad8d
龙飞 <change.long@99bill.com>
2016-12-21 17:16:03 +0800

    feign client demo and hystrix command with ribbon

--- hippo-spring-boot-consumer-ribbon/src/main/java/com/change/spring/cloud/service/RibbonService.java
+++ hippo-spring-boot-consumer-ribbon/src/main/java/com/change/spring/cloud/service/RibbonService.java
@@ -2,7 +2,10 @@ package com.change.spring.cloud.service;
 
 import com.change.spring.cloud.entity.User;
 import com.netflix.appinfo.InstanceInfo;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -15,14 +18,26 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class RibbonService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(RibbonService.class);
+
     @Autowired
     private RestTemplate restTemplate;
 
+    @HystrixCommand(fallbackMethod = "fallback")
     public User findById(Long id) {
         return restTemplate.getForObject("http://hippo-spring-boot/users/" + id, User.class);
     }
 
     public InstanceInfo instanceInfo() {
-        return restTemplate.getForObject("http://hippo-spring-boot/eureka-instance",InstanceInfo.class);
+        return restTemplate.getForObject("http://hippo-spring-boot/eureka-instance", InstanceInfo.class);
+    }
+
+    public User fallback(Long id) {
+        LOGGER.info("异常发生，进入fallback方法，接收的参数 id={}", id);
+        User user = new User();
+        user.setId(-1L);
+        user.setUserName("default username");
+        user.setPassword("123");
+        return user;
     }
 }
