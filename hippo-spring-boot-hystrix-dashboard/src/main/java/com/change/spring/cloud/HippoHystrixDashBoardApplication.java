fd313ec5575025f0313435a9038e62aa3cae4888
龙飞 <change.long@99bill.com>
2017-02-11 13:26:34 +0800

    feature zuul support

--- hippo-spring-boot-hystrix-dashboard/src/main/java/com/change/spring/cloud/HippoHystrixDashBoardApplication.java
+++ hippo-spring-boot-hystrix-dashboard/src/main/java/com/change/spring/cloud/HippoHystrixDashBoardApplication.java
@@ -1,9 +1,11 @@
 package com.change.spring.cloud;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableHystrixDashboard
 public class HippoHystrixDashBoardApplication {
 
