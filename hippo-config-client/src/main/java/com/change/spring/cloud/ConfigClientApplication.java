4b9da50d9886ee7751b3492a48f528a92eac96e5
龙飞 <change.long@99bill.com>
2017-02-23 14:58:23 +0800

    config client support

---
+++ hippo-config-client/src/main/java/com/change/spring/cloud/ConfigClientApplication.java
@@ -0,0 +1,27 @@
+package com.change.spring.cloud;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ConfigClientApplication {
+
+
+    @Value("${profile}")
+    private String profile;
+
+    @GetMapping("/profile")
+    public String getLuckyWord() {
+        return profile;
+    }
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+
+}
