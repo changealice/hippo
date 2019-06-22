2bef8ff1dcefa3daa92c033739d5eeea0bb817ae
changejava <changejava@gmail.com>
2019-06-23 00:40:42 +0800

    guava 集合测试

--- hippo-core/src/main/java/com/change/hippo/guava/ImutableDemo.java
+++ hippo-core/src/main/java/com/change/hippo/guava/ImutableDemo.java
@@ -1,6 +1,8 @@
 package com.change.hippo.guava;
 
+import com.google.common.base.Predicate;
 import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Iterators;
 
 /**
  * 不可更改的集合类
@@ -19,5 +21,15 @@ public class ImutableDemo {
         for (String s : of) {
             System.out.println(s);
         }
+
+        Predicate<String> condition = input -> input.startsWith("P");
+        System.out.println(Iterators.all(of.asList().iterator(), condition));
+
+        String firstElement = Iterators.get(of.iterator(), 0);
+
+        System.out.println(firstElement);
+
+        System.out.println(Iterators.filter(of.iterator(), "B"::equalsIgnoreCase));
+
     }
 }
