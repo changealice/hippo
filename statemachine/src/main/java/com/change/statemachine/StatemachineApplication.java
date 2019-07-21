e459b601afed32e5694b3df7153efa6d00264355
changejava <changejava@gmail.com>
2019-07-21 23:12:30 +0800

    spring statemachine

---
+++ statemachine/src/main/java/com/change/statemachine/StatemachineApplication.java
@@ -0,0 +1,28 @@
+package com.change.statemachine;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.statemachine.StateMachine;
+import org.springframework.statemachine.config.EnableStateMachine;
+
+@SpringBootApplication
+@EnableStateMachine
+public class StatemachineApplication implements CommandLineRunner {
+
+    @Autowired
+    private StateMachine<States, Events> stateMachine;
+
+    public static void main(String[] args) {
+        SpringApplication.run(StatemachineApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        stateMachine.start();
+        stateMachine.sendEvent(Events.PAY);
+        stateMachine.sendEvent(Events.RECEIVE);
+
+    }
+}
